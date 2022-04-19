f1cb4f11676aa294bc3012b7394072855a7a7e1e
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-04-19 11:16:28 +0300

    Названия

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def save_image(image_link, filename):
         file.write(response.content)
 
 
-def get_server_vk(token_vk):
+def get_upload_url(token_vk):
     response = requests.get("https://api.vk.com/method/photos.getWallUploadServer", 
     params={ 
         "v":5.131, 
@@ -49,7 +49,7 @@ def get_server_vk(token_vk):
 
 
 def upload_file_vk(token_vk, filename):
-    url = get_server_vk(token_vk)
+    url = get_upload_url(token_vk)
     with open(f"{filename}", "rb") as file:
         files = {
         'file1': file
